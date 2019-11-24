84e850f9adaed91e9507569911cc3aedc99d7e79
nstrauser <40615455+nstrauser@users.noreply.github.com>
2019-11-23 21:09:47 -0800

    Update mousenow.py

--- mousenow.py
+++ mousenow.py
@@ -1,16 +1,20 @@
-import subprocess
+#!/usr/bin/env python3
+#import subprocess
 import pyautogui
 
-subprocess.Popen(['clear'])
-print('Press Ctrl-C to quit.')
 
-try:
-    while True:
-        #TODO: Get and print the mouse coordinates.
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        print(positionStr, end='')
-        print('\b' * len(positionStr), end='', flush=True)
+def run():
+    try:
+        while True:
+            # Print mouse coordinates
+            x, y = pyautogui.position()
+            positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+            print(positionStr, end='')
+            print('\b' * len(positionStr), end='', flush=True)
+    except KeyboardInterrupt:
+        print('\nDone.')
 
-except KeyboardInterrupt:
-    print('\nDone.')
+if __name__=='__main__':
+    #subprocess.Popen(['clear'])
+    print('\nPress Ctrl-C to quit.\n')
+    run()
